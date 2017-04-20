62831ef32a79d46efcbd8226ef66c7212d242254
Juan Daza <dazajuandaniel@gmail.com>
2017-04-20 14:25:25 +1000

    fixedPython

--- TwitterHarvester/getTweets.py
+++ TwitterHarvester/getTweets.py
@@ -1,141 +1,42 @@
-import re
-import tweepy
+
+import tweepy as tw
 from tweepy import OAuthHandler
-from textblob import TextBlob
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
+import couchdb,json
 
-import couchdb
 
+consumer_key = 'XqNOFK3tkWO3ueraq4WJgAbL8'
+consumer_secret = 'KA79XONJOZL8WkBpZAuqkTjDxRhiT7KGf16x2SLTCFfqSpTnoG'
+access_token = '140966719-NrfyS8phWv3pJAwCQkqoZESAWTQo6AQzfVVYH1Rf'
+access_secret = 'Efi3AuiHq1DTIAfucEFt7SgCZ8rFHsF4Cibk1RJNC95Rt'
 
- 
-class TwitterClient(object):
-    '''
-    Generic Twitter Class for sentiment analysis.
-    '''
-    def __init__(self):
-        '''
-        Class constructor or initialization method.
-        '''
-        # keys and tokens from the Twitter Dev Console
-        consumer_key = 'XqNOFK3tkWO3ueraq4WJgAbL8'
-        consumer_secret = 'KA79XONJOZL8WkBpZAuqkTjDxRhiT7KGf16x2SLTCFfqSpTnoG'
-        access_token = '140966719-NrfyS8phWv3pJAwCQkqoZESAWTQo6AQzfVVYH1Rf'
-        access_secret = 'Efi3AuiHq1DTIAfucEFt7SgCZ8rFHsF4Cibk1RJNC95Rt'
- 
-        # attempt authentication
-        try:
-            # create OAuthHandler object
-            self.auth = OAuthHandler(consumer_key, consumer_secret)
-            # set access token and secret
-            self.auth.set_access_token(access_token, access_token_secret)
-            # create tweepy API object to fetch tweets
-            self.api = tweepy.API(self.auth)
-        except:
-            print("Error: Authentication Failed")
-    
+auth = tw.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+api = tw.API(auth)
+
+try:
+    couch = couchdb.Server()
+    db = couch['raw_tweets']
+except:
+    print 'Database Connection Error'
+
+class StdOutListener(StreamListener):
     def on_data(self, data):
+        
         tweet = json.loads(data)
         db.save(tweet)
+        #print data
         return True
 
     def on_error(self, status):
         print status
- 
-    def clean_tweet(self, tweet):
-        clean={}
-        clean['place']=tweet['place']
-        clean['text']=tweet['text']
-        clean['favorited']=tweet['favorited']
-        clean['lang']=tweet['lang']
-        clean['retweeted']=tweet['retweeted']
-        clean['retweet_count']=tweet['retweet_count']
 
 
-        
-        return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])
-                                    |(\w+:\/\/\S+)", " ", tweet).split())
- 
-    def get_tweet_sentiment(self, tweet):
-        '''
-        Utility function to classify sentiment of passed tweet
-        using textblob's sentiment method
-        '''
-        # create TextBlob object of passed tweet text
-        analysis = TextBlob(self.clean_tweet(tweet))
-        # set sentiment
-        if analysis.sentiment.polarity > 0:
-            return 'positive'
-        elif analysis.sentiment.polarity == 0:
-            return 'neutral'
-        else:
-            return 'negative'
- 
-    def get_tweets(self, query, count = 10):
-        '''
-        Main function to fetch tweets and parse them.
-        '''
-        # empty list to store parsed tweets
-        tweets = []
- 
-        try:
-            # call twitter api to fetch tweets
-            fetched_tweets = self.api.search(q = query, count = count)
- 
-            # parsing tweets one by one
-            for tweet in fetched_tweets:
-                # empty dictionary to store required params of a tweet
-                parsed_tweet = {}
- 
-                # saving text of tweet
-                parsed_tweet['text'] = tweet.text
-                # saving sentiment of tweet
-                parsed_tweet['sentiment'] = self.get_tweet_sentiment(tweet.text)
- 
-                # appending parsed tweet to tweets list
-                if tweet.retweet_count > 0:
-                    # if tweet has retweets, ensure that it is appended only once
-                    if parsed_tweet not in tweets:
-                        tweets.append(parsed_tweet)
-                else:
-                    tweets.append(parsed_tweet)
- 
-            # return parsed tweets
-            return tweets
- 
-        except tweepy.TweepError as e:
-            # print error (if any)
-            print("Error : " + str(e))
- 
-def main():
-    # creating object of TwitterClient Class
-    api = TwitterClient()
-    # calling function to get tweets
-    tweets = api.get_tweets(query = 'Donald Trump', count = 200)
- 
-    # picking positive tweets from tweets
-    ptweets = [tweet for tweet in tweets if tweet['sentiment'] == 'positive']
-    # percentage of positive tweets
-    print("Positive tweets percentage: {} %".format(100*len(ptweets)/len(tweets)))
-    # picking negative tweets from tweets
-    ntweets = [tweet for tweet in tweets if tweet['sentiment'] == 'negative']
-    # percentage of negative tweets
-    print("Negative tweets percentage: {} %".format(100*len(ntweets)/len(tweets)))
-    # percentage of neutral tweets
-    print("Neutral tweets percentage: {} % \
-        ".format(100*len(tweets - ntweets - ptweets)/len(tweets)))
- 
-    # printing first 5 positive tweets
-    print("\n\nPositive tweets:")
-    for tweet in ptweets[:10]:
-        print(tweet['text'])
- 
-    # printing first 5 negative tweets
-    print("\n\nNegative tweets:")
-    for tweet in ntweets[:10]:
-        print(tweet['text'])
- 
-if __name__ == "__main__":
-    # calling main function
-    main()
+if __name__ == '__main__':
+    #This handles Twitter authetification and the connection to Twitter Streaming API
+    l = StdOutListener()
+    stream = Stream(auth, l)    
+    #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
+    stream.filter(locations=[ 141.157913,-38.022041,146.255569,-36.412349])
