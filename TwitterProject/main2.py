d9b77b7d287af46fc83e9d5689cada64c2f2c0d9
zhangw6 <zhangw6@student.unimelb.edu.au>
2017-05-11 00:00:28 +1000

    Update main2.py

--- TwitterProject/main2.py
+++ TwitterProject/main2.py
@@ -119,6 +119,10 @@ def hello6():
     return render_template('index.html', data=str)
 
 
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
      
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
